5a27880f0ede413a9fcf91886e45730d8789501a
Matheus Costa <matheuscostakrenn@gmail.com>
2019-10-10 19:46:44 -0300

    Refeita com ponteiros

--- lab/aula9-C2.c
+++ lab/aula9-C2.c
@@ -1,54 +1,36 @@
 /**
- ** função que Soma valores, da o quadrado e o cubo dela.
+ ** funÃ§Ã£o que Soma valores, da o quadrado e o cubo dela.
 **/
 
 #include <stdio.h>
 
-int Sum(int end){
-    int result=0, i;
-    for(i=1; i<=end; i++)
-        result+=i;
-    return result;
-}
-int Quad(int end){
-    int result=0, i;
-    for(i=1; i<=end; i++)
-        result+=i * i;
-    return result;
-}
-int Cube(int end){
-    int result=0, i;
-    for(i=1; i<=end; i++)
-        result+=i * i * i;
-    return result;
-}
-int SomaValores(int end, int type){
-    int i;
-    if(end<1) return -1;
-    switch(type){
-        case 0:
-            return Sum(end);
-        break;
-        case 1:
-            return Quad(end);
-        break;
-        case 2:
-            return Cube(end);
-        break;
-        default:
-            printf("Erro! Voce usou um numero invalido.");
-            return 100;
-    }
+int SomaValores(int end, int *soma, int *quadrado, int *cubo){
+  int i;
+  if(end<1) return -1;
+  for(i=1; i<=end; i++){
+    *soma+=i;
+    *quadrado+=i * i;
+    *cubo+=i * i * i;
+  }
+  return 0;
 }
 int main(){
-    int init=1, n;
-    printf("Entre o valor de n: ");
-    scanf("%d",&n);
-    /* Fim das açoes do usuario */
-    printf("A soma dos valores entre %d e %d eh: %d\n", init, n, SomaValores(n, 0));
-    printf("A soma dos quadrados dos valores entre %d e %d eh: %d\n",init, n, SomaValores(n, 1));
-    printf("A soma dos cubos dos valores entre %d e %d eh: %d\n", init, n, SomaValores(n, 2));
+  int init=1, n, s=0, q=0, c=0;
 
-    return 0;
+  // Inicio das aÃ§oes do usuario
+  printf("Entre o valor de n: ");
+  scanf("%d",&n);
+
+  // Fim das aÃ§oes do usuario
+
+  if(SomaValores(n, &s, &q, &c)==-1){
+    printf("Digite um numero maior que 0\n\n");
+    return 100;
+  }
+
+  printf("A soma dos valores entre %d e %d eh: %d\n", init, n, s);
+  printf("A soma dos quadrados dos valores entre %d e %d eh: %d\n",init, n, q);
+  printf("A soma dos cubos dos valores entre %d e %d eh: %d\n", init, n, c);
+
+  return 0;
 }
-
