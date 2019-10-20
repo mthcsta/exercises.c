239b600a56bb59f69be79e94a1d40db4260394d6
Matheus Costa <matheuscostakrenn@gmail.com>
2019-10-20 10:30:55 -0300

    mudei o nome da função

--- lab/aula1-2.c
+++ lab/aula1-2.c
@@ -1,10 +1,12 @@
 /**
  ** Era para ser apenas um programa de soma de dois numeros
- ** resolvi praticar um pouco mais e tentar ir um pouco mais além.
+ ** resolvi praticar um pouco mais e tentar ir um pouco mais alÃ©m.
 **/
 #include <stdio.h>
 int multiSum(){
-  int i, limit, values, sum;
+  int i, // ITERATOR 
+      limit, values, // INPUT 
+      sum; // OUTPUT
   puts("Quantos Numeros voce quer somar?");
   scanf("%d",&limit);
   sum = 0;
@@ -15,20 +17,20 @@ int multiSum(){
   }
   return sum;
 }
-int doubleSum(){
+int simpleSum(){
   int val1, val2, // INPUT
-  sum; // OUTPUT
+      sum; // OUTPUT
   printf("Escreva dois valores para fazer a soma deles\n");
   scanf("%d%d", &val1, &val2);
   sum = val1 + val2;
   return sum;
 }
 int main(){
-  int IsDouble,
-  sum; // OUTPUT
+  int IsSimple, // INPUT
+      sum; // OUTPUT
   printf("Escolha o tipo de soma:\n0: soma quantos numeros quiser\n1: soma apenas dois numeros\n");
-  scanf("%d",&IsDouble);
-  sum = IsDouble ? doubleSum() : multiSum();
+  scanf("%d",&IsSimple);
+  sum = IsSimple ? simpleSum() : multiSum();
   printf("\nA soma do valor eh %d",sum);
   return 0;
 }
